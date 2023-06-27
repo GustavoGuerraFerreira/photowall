1a680aa7bc295e6a9cb36b94de33167e129b33d0
Gustavo <gutvasco123@gmail.com>
2023-06-27 12:30:40 -0300

    Trocando os componentes por funções

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
+import React from "react";
 import Photo from "./Photo";
-class Photowall extends Component{
-    render(){
-        return <div className="photoGrid">
-            {this.props.posts.map((post, index) => <Photo post = {post} key = {index}/>)}
-        </div>
-    }
+
+function PhotoWall(props) {
+    return <div className="photoGrid">
+        {props.posts.map((post, index) => <Photo post={post} key={index} />)}
+    </div>
 }
 
-export default Photowall
+
+export default PhotoWall
