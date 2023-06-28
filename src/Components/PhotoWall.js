7bc361196d8ae58fc794bb208cc39c875564fe61
Gustavo <gutvasco123@gmail.com>
2023-06-28 09:36:58 -0300

    PropTypes

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Photo from "./Photo";
+import PropTypes from 'prop-types'
 
 function PhotoWall(props) {
     return <div className="photoGrid">
         {props.posts.map((post, index) => <Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto}/>)}
     </div>
+}
+PhotoWall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+
 }
 
 
